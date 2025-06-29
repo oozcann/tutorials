d2b8840ee242167b843550c5190d025d17f86f89
oozcan <onurozcan95@gmail.com>
2025-06-29 17:58:16 +0300

    Junit - @DisplayName

--- junit/src/test/java/com/junit/CalculatorTest.java
+++ junit/src/test/java/com/junit/CalculatorTest.java
@@ -1,12 +1,15 @@
 package com.junit;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("Calculator Operation Tests")
 public class CalculatorTest {
 
     @Test
+    @DisplayName("Calculator Add Method Testing")
     public void testCalculatorAddMethod () {
         Calculator calculator = new Calculator();
         int resultFromCalculator = calculator.add(10, 20);
@@ -16,6 +19,7 @@ public class CalculatorTest {
     }
 
     @Test
+    @DisplayName("Calculator Subtraction Method Testing")
     public void testCalculatorSubtractionMethod () {
         Calculator calculator = new Calculator();
         int resultFromCalculator = calculator.subtract(10, 20);
