67161f9eae6c8422d860bae06e91c4b38e8ef386
oozcan <onurozcan95@gmail.com>
2025-07-03 20:53:49 +0300

    Stream - FlatMap

--- stream-api/src/main/java/com/stream/api/FlatMap.java
+++ stream-api/src/main/java/com/stream/api/FlatMap.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * flatMap, her bir öğeyi Stream'e dönüştürüp, sonra bu alt Stream'leri düzleştirerek (flatten) tek bir Stream haline getirir.
+ */
 public class FlatMap {
     public static void main(String[] args) {
         List<List<String>> listOfLists = Arrays.asList(
